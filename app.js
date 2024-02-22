7530cf1b147d8e714158d6ea0e19ccc5494d446f
Myeongji An <marvel0855@gmail.com>
2024-02-21 22:13:02 -0600

    hardcoded URI

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const bodyParser = require('body-parser')
+
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: true }))
 
 // use res.render to load up an ejs view file
 
-let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
+let myTypeServer = "Type 1: Reformer";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient("mongodb+srv://abc:jean0855!@cluster0.snv9zih.mongodb.net/?retryWrites=true&w=majority", {
+// make MongoClient with MongoClientOptions object 
+const client = new MongoClient(process.env.URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -28,71 +27,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     const result = await client.db("papa-database").collection("papa-collection").find().toArray();
 
-    //console.log("cxnDB result: ", result);
+    console.log("connect to mongo: ", result);
 
-    return result; 
+    return result;
 
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-//run().catch(console.dir);
 
-app.get('/read', async (req,res) => {
+app.get('/', async (req,res) => {
 
-  let myResultServer = await run(); 
-
-  console.log("myResultServer:", myResultServer);
-
+  let myResultServer = await run();
+  console.log("successfully deployed!");
   res.render('index', {
     myTypeClient: myTypeServer,
     myResultClient: myResultServer
 
   });
 
-
-}); 
-run().catch(console.dir);
-
-
-
-app.get('/', function(req, res) {
-
-  res.render('index', {
-   
-    myTypeClient: myTypeServer 
-
-  });
-  
 });
 
-app.get('/name', (req,res) => {
-
-  console.log("in get to slash name:", req.query.ejsFormName); 
-  myTypeServer = req.query.ejsFormName; 
-
-  res.render('index', {
-    myTypeClient: myTypeServer,
-    myResultClient: "myResultServer"
-
-  });
-
-  
-})
-
-
-
 app.get('/send', function (req, res) {
-  
-    res.send('Hello World from Express <br><a href="/">home</a>')
+    res.send('Hello from the <em>Express</em><br><a href="/"></a>')
 })
 
 app.listen(port, () => {
-console.log(`papa app listening on port ${port}`)
+  console.log(`PAPA is listening on port ${port}`)
 })
